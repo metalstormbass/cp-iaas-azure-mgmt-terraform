1b68a6ce210c74e9c84f8b23a09934911bf3e33c
Michael Braun <michaelbraunbass@gmail.com>
2020-06-22 08:49:04 -0600

    Update

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # cp-iaas-azure-mgmt-terraform
+
+az vm image terms accept --urn checkpoint:check-point-cg-r8040:sg-byol:latest
+
+installer verify Check_Point_R80.40_CME_T83_sk157492.tgz installer install
--- mgmt_bootstrap.sh
+++ mgmt_bootstrap.sh
@@ -1,11 +1,11 @@
-#!/usr/bin/python3 /etc/cloud_config.py
+#This build the config string
+config_string='install_security_gw=false&install_ppak=true&gateway_cluster_member=false&install_security_managment=true&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=MikeNet-cp-gw&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin'
 
+clish -c 'set user admin shell /bin/bash' -s
 
-installationType="management"
-bootstrapScript64=""
-templateName="management"
-templateVersion="20200331"
-isBlink="false"
-osVersion="R8040"
-managementGUIClientNetwork="0.0.0.0/0"
-allowUploadDownload="True"
+config_system.orig -s $config_string
+
+#For future enhancements
+#autoprov_cfg add controller Azure -cn "Azure-Connect" -sb $msft_subscription -en AzureCloud -at $msft_tenantid -aci $msft_clientid -acs $client_secret
+
+reboot
