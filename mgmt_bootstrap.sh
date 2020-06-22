52164457f092d3d37b6cb8772d6dc0c52e432d00
Michael Braun <michaelbraunbass@gmail.com>
2020-06-22 14:10:50 -0600

    Update mgmt_bootstrap.sh

--- mgmt_bootstrap.sh
+++ mgmt_bootstrap.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 #This build the config string
 config_string='install_security_gw=false&install_ppak=true&gateway_cluster_member=false&install_security_managment=true&install_mgmt_primary=true&install_mgmt_secondary=false&download_info=true&hostname=MikeNet-cp-gw&mgmt_gui_clients_radio=any&mgmt_admin_radio=gaia_admin'
 
